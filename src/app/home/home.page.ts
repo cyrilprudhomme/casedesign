2b319ee5af5a585e23a7ab1801e40a90f7d7e37d
cprudhomme <cyrilprudhomme.dev@gmail.com>
2024-09-10 09:14:40 +0200

    refactor code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,6 +13,9 @@ import {
   IonContent,
   IonGrid,
   IonHeader,
+  IonInput,
+  IonItem,
+  IonList,
   IonMenuButton,
   IonRouterLink,
   IonRow,
@@ -31,13 +34,36 @@ import {ToolbarComponent} from "../components/toolbar/toolbar.component";
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, FooterComponent, IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonText, IonRouterLink, RouterLink, IonButton, ToolbarComponent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, FooterComponent, IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonText, IonRouterLink, RouterLink, IonButton, ToolbarComponent, IonInput, IonItem, IonList]
 })
 export class HomePage implements OnInit {
   protected readonly appPages = appPages;
   private apiService = inject(ApiService);
   posts = this.apiService.posts
-
+  offers = [
+    {
+      title: "ÉQUILIBRE DÉCO",
+      subtitle: "À partir de 300€",
+      image_url: "https://casedesign.fr/content/images/2024/05/salonsejour2.jpeg",
+      body: "Je vous propose deux heures de coaching décoration, à domicile, en café ou en Visio ,afin de vous conseiller sur mes idées, mes inspirations, les dernières tendances et les bonnes adresses de décoration."
+    },
+    {
+      title: "CRÉATIVITÉ HABITAT",
+      subtitle: "À partir de 600€",
+      image_url: "https://casedesign.fr/content/images/2024/05/entr-3.jpeg",
+      body: "Vous avez une pièce dont la décoration ou l'aménagement vous pose problème ?\n" +
+        "On se donne un rendez vous, on échange sur le souci que vous rencontrez, je prend des mesures, je vous interroge sur vos goûts et vos envies.\n" +
+        "À l'issue du rendez-vous, je vous livre un document d'analyse du projet avec un visuel 3D pour vous aider à vous projeter et une planche d'ambiance pour votre pièce à décorer avec toutes les références dont vous aurez besoin."
+    },
+    {
+      title: "ÉLÉGANCE SUR MESURE",
+      subtitle: "Sur devis",
+      image_url: "https://casedesign.fr/content/images/2024/05/entr-3.jpeg",
+      body: "Vous avez une pièce dont la décoration ou l'aménagement vous pose problème ?\n" +
+        "On se donne un rendez vous, on échange sur le souci que vous rencontrez, je prend des mesures, je vous interroge sur vos goûts et vos envies.\n" +
+        "À l'issue du rendez-vous, je vous livre un document d'analyse du projet avec un visuel 3D pour vous aider à vous projeter et une planche d'ambiance pour votre pièce à décorer avec toutes les références dont vous aurez besoin."
+    }
+  ]
   constructor() {
 
   }
--- src/app/services/reference.ts
+++ src/app/services/reference.ts
@@ -2,6 +2,6 @@ export const appPages = [
   {title: 'Accueil', url: '/home', icon: 'home'},
   {title: 'Posts', url: '/posts', icon: 'albums'},
   {title: 'Qui suis-je ?', url: '/me', icon: 'help'},
-  {title: 'Mes offres', url: '/offers', icon: 'pricetags'},
-  {title: 'Contact', url: '/contact', icon: 'mail-open'},
+  /*  {title: 'Mes offres', url: '/offers', icon: 'pricetags'},
+    {title: 'Contact', url: '/contact', icon: 'mail-open'},*/
 ];
