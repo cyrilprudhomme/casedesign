7f6e15273fc3b98730f700d58fe19de169ad5937
cprudhomme <cprudhomme>
2024-09-10 10:59:38 +0200

    init server

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,16 @@ import {IonicRouteStrategy, provideIonicAngular} from '@ionic/angular/standalone
 
 import {routes} from './app/app.routes';
 import {AppComponent} from './app/app.component';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
   providers: [
     {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes, withPreloading(PreloadAllModules)), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 });
