f9d3683a7e3ac4f31ac954f1a92e87b38767e41e
cprudhomme <cyrilprudhomme.dev@gmail.com>
2024-09-15 11:19:10 +0200

    Fix Reorg

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -6,6 +6,13 @@ interface Post {
   subtitle: string;
   title: string;
   body: string
+  albums: Albums[]
+}
+
+interface Albums {
+  title: string;
+  subtitle: string;
+  description: string
   images: Image[]
 }
 
@@ -21,19 +28,19 @@ interface Image {
 })
 export class ApiService {
   posts = signal<Post[]>([{
-    at_first: false, title: '', subtitle: '', image_url: '', body: '', images: [{
+    at_first: false, title: '', subtitle: '', image_url: '', body: '', albums: [{
       title: '',
       subtitle: '',
       description: '',
-      url: ''
+      images: []
     }]
   }])
   post = signal<Post>({
-    at_first: false, title: '', subtitle: '', image_url: '', body: '', images: [{
+    at_first: false, title: '', subtitle: '', image_url: '', body: '', albums: [{
       title: '',
       subtitle: '',
       description: '',
-      url: ''
+      images: []
     }]
   })
 
@@ -49,52 +56,60 @@ export class ApiService {
         subtitle: "28 Mai 2024",
         image_url: "https://casedesign.fr/content/images/2024/05/salonsejour2.jpeg",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        images: [
+        albums: [
           {
             title: '',
             subtitle: '',
             description: '',
-            url: 'https://casedesign.fr/content/images/2024/05/salonsejour2.jpeg'
-          },
-          {title: '', subtitle: '', description: '', url: 'https://casedesign.fr/content/images/2024/05/sam4.jpeg'},
-          {title: '', subtitle: '', description: '', url: 'https://casedesign.fr/content/images/2024/05/sma5.jpeg'},
-          {
-            title: '',
-            subtitle: '',
-            description: '',
-            url: 'https://casedesign.fr/content/images/2024/05/salon-sejour-4.jpeg'
-          },
-          {
-            title: '',
-            subtitle: '',
-            description: '',
-            url: 'https://casedesign.fr/content/images/2024/05/salon-sejour5.jpeg'
-          },
-          {
-            title: '',
-            subtitle: '',
-            description: '',
-            url: 'https://casedesign.fr/content/images/2024/05/salon-sejour-6.jpeg'
-          },
-          {
-            title: '',
-            subtitle: '',
-            description: '',
-            url: 'https://casedesign.fr/content/images/2024/05/salon-sejour-8.jpeg'
-          },
-          {
-            title: '',
-            subtitle: '',
-            description: '',
-            url: 'https://casedesign.fr/content/images/2024/05/salon-sejour9.jpeg'
-          },
-          {
-            title: '',
-            subtitle: '',
-            description: '',
-            url: 'https://casedesign.fr/content/images/2024/05/salon-sejour-11.jpeg'
+            images: [
+              {
+                title: '',
+                subtitle: '',
+                description: '',
+                url: 'https://casedesign.fr/content/images/2024/05/salonsejour2.jpeg'
+              },
+              {title: '', subtitle: '', description: '', url: 'https://casedesign.fr/content/images/2024/05/sam4.jpeg'},
+              {title: '', subtitle: '', description: '', url: 'https://casedesign.fr/content/images/2024/05/sma5.jpeg'},
+              {
+                title: '',
+                subtitle: '',
+                description: '',
+                url: 'https://casedesign.fr/content/images/2024/05/salon-sejour-4.jpeg'
+              },
+              {
+                title: '',
+                subtitle: '',
+                description: '',
+                url: 'https://casedesign.fr/content/images/2024/05/salon-sejour5.jpeg'
+              },
+              {
+                title: '',
+                subtitle: '',
+                description: '',
+                url: 'https://casedesign.fr/content/images/2024/05/salon-sejour-6.jpeg'
+              },
+              {
+                title: '',
+                subtitle: '',
+                description: '',
+                url: 'https://casedesign.fr/content/images/2024/05/salon-sejour-8.jpeg'
+              },
+              {
+                title: '',
+                subtitle: '',
+                description: '',
+                url: 'https://casedesign.fr/content/images/2024/05/salon-sejour9.jpeg'
+              },
+              {
+                title: '',
+                subtitle: '',
+                description: '',
+                url: 'https://casedesign.fr/content/images/2024/05/salon-sejour-11.jpeg'
+              }
+            ]
           }
-        ]
+        ],
+
       },
       {
         at_first: true,
@@ -102,44 +117,51 @@ export class ApiService {
         subtitle: "29 Avril 2024",
         image_url: "https://casedesign.fr/content/images/2024/05/entr-3.jpeg",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        images: [
+        albums: [
           {
             title: '',
             subtitle: '',
             description: '',
-            url: 'https://casedesign.fr/content/images/2024/04/3FCB0741-43CB-4260-8B00-6146D1814801.png'
-          },
-          {
-            title: '',
-            subtitle: '',
-            description: '',
-            url: 'https://casedesign.fr/content/images/2024/04/5657238F-7C3C-4BD1-95B2-6BD5E8F520FA.png'
-          },
-          {
-            title: '',
-            subtitle: '',
-            description: '',
-            url: 'https://casedesign.fr/content/images/2024/04/E81B3EBB-AA71-4E63-83C9-9660A4A18B75.png'
-          },
-          {
-            title: '',
-            subtitle: '',
-            description: '',
-            url: 'https://casedesign.fr/content/images/2024/04/2886C500-2C2D-4069-B9E1-55DF3AC95CEA.png'
-          },
-          {
-            title: '',
-            subtitle: '',
-            description: '',
-            url: 'https://casedesign.fr/content/images/2024/04/D2422471-71E8-49CA-8465-611B8BAD5CA3.png'
-          },
-          {
-            title: '',
-            subtitle: '',
-            description: '',
-            url: 'https://casedesign.fr/content/images/2024/04/E917EECF-AF52-40A3-B21D-5A245BE61B73.png'
-          },
-        ]
+            images: [
+              {
+                title: '',
+                subtitle: '',
+                description: '',
+                url: 'https://casedesign.fr/content/images/2024/04/3FCB0741-43CB-4260-8B00-6146D1814801.png'
+              },
+              {
+                title: '',
+                subtitle: '',
+                description: '',
+                url: 'https://casedesign.fr/content/images/2024/04/5657238F-7C3C-4BD1-95B2-6BD5E8F520FA.png'
+              },
+              {
+                title: '',
+                subtitle: '',
+                description: '',
+                url: 'https://casedesign.fr/content/images/2024/04/E81B3EBB-AA71-4E63-83C9-9660A4A18B75.png'
+              },
+              {
+                title: '',
+                subtitle: '',
+                description: '',
+                url: 'https://casedesign.fr/content/images/2024/04/2886C500-2C2D-4069-B9E1-55DF3AC95CEA.png'
+              },
+              {
+                title: '',
+                subtitle: '',
+                description: '',
+                url: 'https://casedesign.fr/content/images/2024/04/D2422471-71E8-49CA-8465-611B8BAD5CA3.png'
+              },
+              {
+                title: '',
+                subtitle: '',
+                description: '',
+                url: 'https://casedesign.fr/content/images/2024/04/E917EECF-AF52-40A3-B21D-5A245BE61B73.png'
+              },
+            ]
+          }
+        ],
       }
     ]
   }
